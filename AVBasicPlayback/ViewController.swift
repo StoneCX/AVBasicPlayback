5756b4474ce82d50dd083aaf5e86f190fca04897
StoneCX <chengxcv5@163.com>
2017-06-09 20:19:13 +0800

    add a simple AVPlayerController

--- AVBasicPlayback/ViewController.swift
+++ AVBasicPlayback/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,23 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	@IBAction func playVideo(_ sender: Any) {
+		
+		guard let url = URL(string: "https://devimages.apple.com.edgekey.net/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8") else {
+			return
+		}
+		
+		let player = AVPlayer(url: url)
+		
+		let controller = AVPlayerViewController()
+		controller.player = player
+		
+		present(controller, animated: true) {
+			player.play()
+		}
+	
+	
+	}
+	
 }
 
